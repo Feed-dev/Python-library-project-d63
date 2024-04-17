d2d8976c8277d2d5bbb26891dca4d60ba6249e73
federico Lubbert <federico.lubbert@gmail.com>
2024-04-17 11:26:32 +0200

    half way done

--- main.py
+++ main.py
@@ -1,33 +1,39 @@
 from flask import Flask, render_template, request, redirect, url_for
-
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, IntegerField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key_here'
 
 all_books = []
 
 
-@app.route('/')
+class BookForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    author = StringField('Author', validators=[DataRequired()])
+    rating = IntegerField('Rating', validators=[DataRequired()])
+    submit = SubmitField('Add Book')
+
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    pass
+    return render_template('index.html', books=all_books)
 
 
-@app.route("/add")
+@app.route("/add", methods=['GET', 'POST'])
 def add():
-    pass
+    form = BookForm()
+    if form.validate_on_submit():
+        new_book = {
+            "title": form.title.data,
+            "author": form.author.data,
+            "rating": form.rating.data
+        }
+        all_books.append(new_book)
+        return redirect(url_for('home'))
+    return render_template('add.html', form=form)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
